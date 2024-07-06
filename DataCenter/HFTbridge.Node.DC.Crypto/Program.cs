96a77006e2b5f77489aabb683c160f611e7140cc
MikePapinski <mp.papinski@gmail.com>
2024-07-06 03:45:51 +0200

    test

--- DataCenter/HFTbridge.Node.DC.Crypto/Program.cs
+++ DataCenter/HFTbridge.Node.DC.Crypto/Program.cs
@@ -3,104 +3,112 @@ using Microsoft.Extensions.Logging;
 using Serilog;
 using System;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http.Connections;  // Add this using directive
-using Microsoft.AspNetCore.SignalR.Protocol;
+using Microsoft.AspNetCore.Http.Connections;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using System.Threading;
 
-namespace HFTbridge.Agent;
-class Program
-{   
-    private static string Version = Assembly.GetEntryAssembly()?.GetName().Version?.ToString() ?? "Unknown";
-    private static string SchemaVersion = Assembly.GetEntryAssembly()?.GetName().Version?.ToString() ?? "Unknown";
-    private static string SharedNodeVersion = Assembly.GetEntryAssembly()?.GetName().Version?.ToString() ?? "Unknown";
-    private const string NODE_ORGANIZATION_ID = "PUBLIC";
-    private const string NODE_SECRET = "B396A51A5D3EDA7CBB2B7FF8A662F";
-    private const string NODE_ID = "CRYPTO";
-    private const string URL_SERVICE = "https://hub.dc.hft-app.net";
-    private const string URL_FORMAT = "{0}/node-mothership?nodeId={1}&version={2}&schemaVersion={3}&sharedNodeVersion={4}&secret={5}&organizationId={6}";
-    // private const string URL_HUB = "http://localhost:5242";
-
-    private static string MothershipURL = $"{string.Format(URL_FORMAT, URL_SERVICE, NODE_ID, Version, SchemaVersion, SharedNodeVersion, NODE_SECRET, NODE_ORGANIZATION_ID)}";
-
-
-    private const string URL_AGENT_TEST= "https://hub.dc.hft-app.net/node-mothership?agentId=1";
-
-    static async Task Main(string[] args)
+namespace HFTbridge.Agent
+{
+    class Program
     {
-        ConfigureLogger();
+        private static string Version = Assembly.GetEntryAssembly()?.GetName().Version?.ToString() ?? "Unknown";
+        private static string SchemaVersion = Assembly.GetEntryAssembly()?.GetName().Version?.ToString() ?? "Unknown";
+        private static string SharedNodeVersion = Assembly.GetEntryAssembly()?.GetName().Version?.ToString() ?? "Unknown";
+        private const string NODE_ORGANIZATION_ID = "PUBLIC";
+        private const string NODE_SECRET = "B396A51A5D3EDA7CBB2B7FF8A662F";
+        private const string NODE_ID = "CRYPTO";
+        private const string URL_SERVICE = "https://hub.dc.hft-app.net";
+        private const string URL_FORMAT = "{0}/node-mothership?nodeId={1}&version={2}&schemaVersion={3}&sharedNodeVersion={4}&secret={5}&organizationId={6}";
+        private static string MothershipURL = $"{string.Format(URL_FORMAT, URL_SERVICE, NODE_ID, Version, SchemaVersion, SharedNodeVersion, NODE_SECRET, NODE_ORGANIZATION_ID)}";
 
-        var agentHubClient = BuildHubClient(URL_FORMAT);
-        await agentHubClient.StartAsync();
-
-        // 6. Keep the program running
-        Log.Logger.Information("< --- HFTbridge Data Center is running --- >");
-        while (true)
+        static async Task Main(string[] args)
         {
-            await Task.Delay(1000);
-        }
-    }
+            ConfigureLogger();
 
-    private static void ConfigureLogger()
-    {
-        var loggerConfiguration = new LoggerConfiguration();
+            var agentHubClient = BuildHubClient(MothershipURL);
 
-        #if DEBUG
-        loggerConfiguration
-            .MinimumLevel.Debug() // Set minimum log level to Debug for verbose logging
-            .WriteTo.Console();   // Write logs to console in debug mode
-        #else
-        loggerConfiguration
-            .MinimumLevel.Information() // Set minimum log level to Information in release mode
-            .WriteTo.File("hftbridge-agent-log-.txt", rollingInterval: RollingInterval.Day); // Write logs to file in release mode
-        #endif
+            // // Attach the event handler
+            // agentHubClient.On<long, string, string, string, string, byte[], string, string, string>("HandleNewMessage", (ts, severity, actionId, exchange, messageType, @event, userId, nodeId, mail) =>
+            // {
+            //     ReceiveMessage(ts, severity, actionId, exchange, messageType, @event, userId, nodeId, mail);
+            // });
 
-        Log.Logger = loggerConfiguration.CreateLogger();
-        Log.Logger.Debug(" <--- Starting DC LOGGERt --->");
-    }
+            await agentHubClient.StartAsync();
 
-
-    private static HubConnection BuildHubClient(string urlFormat)
-    {
-        Console.WriteLine(MothershipURL);
-        var hubClient = new HubConnectionBuilder()
-            .WithUrl(MothershipURL, options =>
+            // Keep the program running
+            Log.Logger.Information("< --- HFTbridge Data Center is running --- >");
+            while (true)
             {
-                options.Transports = HttpTransportType.WebSockets;
-            })
-            // .ConfigureLogging(logging => {
-            //     // Bind serilog here logging
-            //     logging.AddSerilog(); // Add this line
-            // })
-            .AddMessagePackProtocol() 
-            .Build();
-
-        hubClient.Closed += async (error) =>
-        {
-            Log.Logger.Error("SERIOUS SIGNAL DISCONNECT {error}", error.Message);
-            ReconnectAgent(hubClient);
-        };
-        return hubClient;
-    }
-
-    private static async void ReconnectAgent(HubConnection hubClient)
-    {
-        var timeout = 0;
-        while (hubClient.State != HubConnectionState.Connected)
-        {
-            await hubClient.StartAsync();
-            Thread.Sleep(1000);
-            timeout ++;
-            Log.Logger.Warning($"SERIOUS SIGNAL R TIMEOUT : {timeout}");
-            if(timeout > 10)
-            {
-                Log.Logger.Error("SERIOUS SIGNAL R TIMEOUT FINAL !");
-                return;
+                await Task.Delay(1000);
             }
+        }
 
+        private static void ReceiveMessage(long ts, string severity, string actionId, string exchange, string messageType, byte[] @event, string userId, string nodeId, string mail)
+        {
+            Log.Logger.Information("Received: {messageType}", messageType);
+        }
+
+        private static void ConfigureLogger()
+        {
+            var loggerConfiguration = new LoggerConfiguration();
+
+#if DEBUG
+            loggerConfiguration
+                .MinimumLevel.Debug() // Set minimum log level to Debug for verbose logging
+                .WriteTo.Console();   // Write logs to console in debug mode
+#else
+            loggerConfiguration
+                .MinimumLevel.Information() // Set minimum log level to Information in release mode
+                .WriteTo.File("hftbridge-agent-log-.txt", rollingInterval: RollingInterval.Day); // Write logs to file in release mode
+#endif
+
+            Log.Logger = loggerConfiguration.CreateLogger();
+            Log.Logger.Debug(" <--- Starting DC LOGGER --->");
+        }
+
+        private static HubConnection BuildHubClient(string url)
+        {
+            Console.WriteLine(url);
+            var hubClient = new HubConnectionBuilder()
+                .WithUrl(url, options =>
+                {
+                    options.Transports = HttpTransportType.WebSockets;
+                })
+                .AddMessagePackProtocol()
+                .Build();
+
+            hubClient.Closed += async (error) =>
+            {
+                Log.Logger.Error("SERIOUS SIGNAL DISCONNECT {error}", error.Message);
+                await ReconnectAgent(hubClient);
+            };
+            return hubClient;
+        }
+
+        private static async Task ReconnectAgent(HubConnection hubClient)
+        {
+            var timeout = 0;
+            while (hubClient.State != HubConnectionState.Connected)
+            {
+                try
+                {
+                    await hubClient.StartAsync();
+                }
+                catch (Exception ex)
+                {
+                    Log.Logger.Error("Reconnection attempt failed: {message}", ex.Message);
+                }
+
+                await Task.Delay(1000);
+                timeout++;
+                Log.Logger.Warning($"SERIOUS SIGNAL R TIMEOUT : {timeout}");
+                if (timeout > 10)
+                {
+                    Log.Logger.Error("SERIOUS SIGNAL R TIMEOUT FINAL !");
+                    return;
+                }
+            }
         }
     }
-    
 }
-
-
-
